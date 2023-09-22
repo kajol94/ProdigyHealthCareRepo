d8122fdb07cdf0d854fbdd354230c02ef56778e7
Kajol <kajol43994@gmail.com>
2023-09-22 20:12:55 +0530

    commited on 22-09-2023

--- Prodigy/src/main/java/com/bk_pageObject/ClaimAdjusterManageClaim.java
+++ Prodigy/src/main/java/com/bk_pageObject/ClaimAdjusterManageClaim.java
@@ -1,32 +1,13 @@
 package com.bk_pageObject;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.io.UnsupportedEncodingException;
-import java.net.URL;
-import java.nio.charset.Charset;
 import java.util.List;
 
-import org.apache.commons.io.IOUtils;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.log4j.Logger;
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.testng.Assert;
 
 import com.aventstack.extentreports.Status;
 import com.bk.testbase.TestBase;
@@ -46,14 +27,8 @@ public class ClaimAdjusterManageClaim {
 	@FindBy(xpath = "//*[@title=\"Manage Claim\"]")
 	WebElement manageClaimMenu;
 
-	@FindBy(xpath="//td[@data-attribute=\"prd_group\"]")
-	WebElement claimGroupValue;
-	
-	@FindBy(id="prd_sourceorganization")
-	WebElement sourceOrganizationValue;
-	
-	@FindBy(xpath = "//input[@aria-label=\"First Name\"]")
-	WebElement patientFname;
+	@FindBy(xpath = "//td[@data-attribute=\"prd_group\"]")
+	List<WebElement> groupId;
 
 	@FindBy(xpath = "//input[@id=\"user-role\"]")
 	WebElement userRole;
@@ -62,75 +37,28 @@ public class ClaimAdjusterManageClaim {
 	WebElement getUserName;
 
 	@FindBy(xpath = "//tr/td/child::a")
-	List<WebElement> claimList;
+	List<WebElement> listOfClaims;
 
-	@FindBy(xpath = "//li/a[@aria-label=\"Manage\"]/following-sibling::ul/li/child::a")
-	List<WebElement> listOfSubMenuOfManageMenu;
+	@FindBy(id = "prd_claimadjuster")
+	WebElement claimAdjustId;
 
-	@FindBy(xpath = "//a[@aria-label=\"User Request List\"]")
-	WebElement userRequestListMenu;
+	@FindBy(xpath = "//div[@class =\"form-readonly entity-form\"]")
+	WebElement checkReadOnly;
 
-	@FindBy(xpath = "//a[@aria-label=\"Manage User\"]")
-	WebElement manageUserMenu;
-
-	@FindBy(xpath = "//a[@aria-label=\"Manage Ownership\"]")
-	WebElement manageOwnershipMenu;
-
-	@FindBy(xpath = "//span[@class=\"username\"]")
-	WebElement profileName;
-
-	@FindBy(xpath = "//a[@title=\"Sign out\"]")
-	WebElement logout;
-
-	@FindBy(xpath = "//h1[@id=\"external-login-heading\"]")
-	WebElement signoutPage;
-
-	@FindBy(id = "prd_emailid")
-	WebElement emailAddress;
-
-	@FindBy(xpath = "//ul[@class=\"nav nav-tabs\"]/li[2]/child::a")
-	WebElement claimInformationTab;
-
-	@FindBy(xpath = "//tr[@data-entity=\"prd_claiminformations\"]")
-	List<WebElement> getListOfClaims;
-
-	@FindBy(xpath = "//a[text()=\"Patient - Prescription Details\"]")
-	WebElement prescriptionTab;
-
-	@FindBy(xpath = "//tr[@data-entity=\"prd_prescriptionview\"]")
-	List<WebElement> getListOfPrescription;
-	
-	
-	
-	public String getHTMLResponse(String url){
-		JavascriptExecutor js = (JavascriptExecutor) driver;
-	      String body = (String)js.executeAsyncScript(
-	            "var callback = arguments[arguments.length - 1];" +
-	                    "var xhr = new XMLHttpRequest();" +
-	                    "xhr.open('GET', '"+ url + "', true);" +
-	                    "xhr.onreadystatechange = function() {" +
-	                    "  if (xhr.readyState == 4) {" +
-	                    "    callback(xhr.responseText);" +
-	                    "  }" +
-	                    "};" +
-	                    "xhr.send();");
-	      return body;
-	  }
-	
-	
 	public ClaimAdjusterManageClaim(WebDriver driver) {
 		this.driver = driver;
 		PageFactory.initElements(driver, this);
 		waitHelper = new WaitHelper(driver);
 		TestBase.logExtentReport("Manage Patient page object has been created");
 	}
-	
-	
 
-	public void clickOnClaimInformationTab() {
-		log.info("clicking on claim Information tab...");
-		logExtentReport("clicking on submit button...");
-		this.claimInformationTab.click();
+	public String getHTMLResponse(String url) {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		String body = (String) js.executeAsyncScript("var callback = arguments[arguments.length - 1];"
+				+ "var xhr = new XMLHttpRequest();" + "xhr.open('GET', '" + url + "', true);"
+				+ "xhr.onreadystatechange = function() {" + "  if (xhr.readyState == 4) {"
+				+ "    callback(xhr.responseText);" + "  }" + "};" + "xhr.send();");
+		return body;
 	}
 
 	public void clickOnManageMenu() {
@@ -140,65 +68,10 @@ public class ClaimAdjusterManageClaim {
 		this.manageMenu.click();
 	}
 
-	public void clickOnPrescriptionTab() {
-		log.info("clicking on prescription tab...");
-		logExtentReport("clicking on prescription tab...");
-		this.prescriptionTab.click();
-	}
-
 	public void clickOnManageClaimMenu() {
 		log.info("Clicking on manage claim button...");
 		logExtentReport("Clicking on manage claim button...");
 		this.manageClaimMenu.click();
-	}
-
-	public List<WebElement> getListOfClaim() {
-		log.info("Getting the list of claim record");
-		logExtentReport("Getting the list of claim record");
-		return claimList;
-	}
-
-	public List<WebElement> getListOfSubMenu() {
-		log.info(
-				"Get the list of sub menus and check the visiblity of Manage User, Manage Ownership, User Request List.");
-		logExtentReport(
-				"Get the list of sub menus and check the visiblity of Manage User, Manage Ownership, User Request List.");
-		return listOfSubMenuOfManageMenu;
-	}
-
-	public List<WebElement> getListOfPatientClaim() {
-		log.info("Getting the list of patient claim");
-		logExtentReport("Getting the list of patient claim");
-		return getListOfClaims;
-	}
-
-	public List<WebElement> getListOfPrescription() {
-		log.info("Getting the list of patient prescription");
-		logExtentReport("Getting the list of patient prescription");
-		return getListOfPrescription;
-	}
-
-	public boolean verifyUserRequestListMenuVisibility() {
-		log.info("Check visibility of User Request List Menu...");
-		logExtentReport("Check visibility of User Request List Menu...");
-		return new VerificationHelper(driver).isNotDisplayed(userRequestListMenu);
-	}
-
-	public boolean verifyManageUserMenuVisibility() {
-		log.info("Check visiblity of Manage User Menu...");
-		logExtentReport("Check visiblity of Manage User Menu...");
-		return new VerificationHelper(driver).isNotDisplayed(manageUserMenu);
-	}
-
-	public boolean verifyManageOwnershipMenuVisibility() {
-		log.info("Check visiblity of Manage Ownership Menu...");
-		logExtentReport("Check visiblity of Manage Ownership Menu...");
-		return new VerificationHelper(driver).isNotDisplayed(manageOwnershipMenu);
-	}
-
-	public String verifyLoggedInUserRole() {
-		String getUserRole = userRole.getAttribute("value");
-		return getUserRole;
 	}
 
 	public String getLoggedInUserName() {
@@ -207,14 +80,32 @@ public class ClaimAdjusterManageClaim {
 		return new VerificationHelper(driver).getText(getUserName);
 	}
 
-	public String getGroupName() {
-		String getGroupNameId = claimGroupValue.getAttribute("data-value");
-		return getGroupNameId;
+	public List<WebElement> getGroupId() {
+		return groupId;
 	}
-	
-	public String getSourceOrganization() {
-		String sourceOrgId = sourceOrganizationValue.getAttribute("value");
-		return sourceOrgId;
+
+	public List<WebElement> getListofClaims() {
+		return listOfClaims;
+	}
+
+	public String getClaimAdjusterId() {
+		String claimAdjusterId = claimAdjustId.getAttribute("value");
+		return claimAdjusterId;
+	}
+
+	public boolean isAttribtuePresent() {
+		boolean result = false;
+
+		try {
+			String value = this.checkReadOnly.getAttribute("readonly");
+			if (value != null) {
+				result = true;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return result;
 	}
 
 	public void verifyManageClaimView() {
@@ -222,30 +113,18 @@ public class ClaimAdjusterManageClaim {
 		clickOnManageClaimMenu();
 	}
 
-	public String getPatientFirstName() {
-		String getFirstName = patientFname.getAttribute("value");
-		return getFirstName;
+	public void logExtentReport(String s1) {
+		TestBase.test.log(Status.INFO, s1);
 	}
 
-	public void clickOnProfileIconButton() {
+	/*public void clickOnProfileIconButton() {
 		log.info("clicking on profile icon button...");
 		logExtentReport("clicking on profile icon button...");
-		this.profileName.click();
-	}
-
-	public void clickOnLogoutButton() {
-		log.info("clicking on logout button...");
-		logExtentReport("clicking on logout button...");
-		waitHelper.waitForElement(logout, 10);
-		this.logout.click();
+		this.getUserName.click();
 	}
 
 	public boolean verifySuccessLogOut() {
 		return new VerificationHelper(driver).isDisplayed(signoutPage);
-	}
-
-	public void logExtentReport(String s1) {
-		TestBase.test.log(Status.INFO, s1);
 	}
 
 	public boolean verifySuccessLogin() {
@@ -277,6 +156,6 @@ public class ClaimAdjusterManageClaim {
 		Assert.assertEquals(userRole.isDisplayed(), result);
 		logExtentReport("User LogOut successfully....");
 
-	}
+	}*/
 
 }
--- Prodigy/src/test/java/com/bk/testScripts/loginPage/ClaimAdjusterManageClaimTest.java
+++ Prodigy/src/test/java/com/bk/testScripts/loginPage/ClaimAdjusterManageClaimTest.java
@@ -1,11 +1,15 @@
 package com.bk.testScripts.loginPage;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -15,85 +19,124 @@ import com.bk_helper.browserconfiguration.config.ObjectReader;
 import com.bk_pageObject.ClaimAdjusterManageClaim;
 import com.bk_pageObject.LoginPage;
 
-
 public class ClaimAdjusterManageClaimTest extends TestBase {
 
 	String userRole = null;
+	String userId = null;
 	String getUserName = null;
 	String claimAdjGroupValue = null;
 	String patientGroupValue = null;
 
-	/*
-	 * @Test public void json() throws JSONException, IOException { loginPage = new
-	 * LoginPage(driver);
-	 * loginPage.loginToApplication(ObjectReader.reader.getUserName(),
-	 * ObjectReader.reader.getPassword());
-	 * 
-	 * manageClaim = new ClaimAdjusterManageClaim(driver);
-	 * manageClaim.verifyManageClaimView(); String url =
-	 * "https://prodigyservicesdev.powerappsportals.com/user-details/";
-	 * 
-	 * String keyValue = manageClaim.getHTMLResponse(url); JSONObject jsonObject =
-	 * new JSONObject(keyValue); JSONArray tsmresponse = (JSONArray)
-	 * jsonObject.get("groups"); for(int i=0; i<tsmresponse.length(); i++){ String
-	 * getGroupId = tsmresponse.get(i).toString(); System.out.println(getGroupId); }
-	 * 
-	 * 
-	 * 
-	 * }
-	 */
+	/*@Test
+	public void json() throws JSONException, IOException {
+		loginPage = new LoginPage(driver);
+		loginPage.loginToApplication(ObjectReader.reader.getUserName(), ObjectReader.reader.getPassword());
 
-	@Test(priority = 0, description = "Verify list of claim records in manage claim page")
-	public void verifyListOfClaimRecord() {
+		manageClaim = new ClaimAdjusterManageClaim(driver); //
+		manageClaim.verifyManageClaimView();
+		String url = "https://prodigyservicesdev.powerappsportals.com/user-details/";
+
+		String keyValue = manageClaim.getHTMLResponse(url);
+		JSONObject jsonObject = new JSONObject(keyValue);
+		JSONArray tsmresponse = (JSONArray) jsonObject.get("groups");
+		for (int i = 0; i < tsmresponse.length(); i++) {
+			ArrayList<String> groupList = new ArrayList<String>();
+			boolean s = groupList.add(tsmresponse.get(i).toString());
+			if (s) {
+				System.out.println(groupList);
+			}
+		}
+
+	}*/
+
+	@Test(priority = 1, description = "Verify edit option based on the Claim Adjuster value avaialble in claim details")
+	public void verifyEditClaimRecords() {
+
 		loginPage = new LoginPage(driver);
 		loginPage.loginToApplication(ObjectReader.reader.getUserName(), ObjectReader.reader.getPassword());
 
 		manageClaim = new ClaimAdjusterManageClaim(driver);
-
 		manageClaim.verifyManageClaimView();
 
 		getUserName = manageClaim.getLoggedInUserName();
 
 		String url = "https://prodigyservicesdev.powerappsportals.com/user-details/";
-
 		String keyValue = manageClaim.getHTMLResponse(url);
 		JSONObject jsonObject = new JSONObject(keyValue);
 
-		// String claimAdjSourcOrgValue = jsonObject.get("sourceOrg").toString();
 		userRole = jsonObject.get("webRole").toString();
-		JSONArray tsmresponse = (JSONArray) jsonObject.get("groups");
 		if (userRole.contains("Claim Adjuster")) {
-			for (int i = 0; i < tsmresponse.length(); i++) {
-				claimAdjGroupValue = tsmresponse.get(i).toString();
-				System.out.println(claimAdjGroupValue);
 
-				List<WebElement> list = manageClaim.getListOfClaim();
+			List<WebElement> getClaimList = manageClaim.getListofClaims();
+			for (int j = 0; j < getClaimList.size(); j++) {
+				getClaimList.get(j).click();
+				userId = jsonObject.get("userId").toString();
+				String claimAdjusterid = manageClaim.getClaimAdjusterId();
 
-				for (int j = 0; j < list.size(); j++) {
-					String patientGroup = manageClaim.getGroupName();
-					Pattern p = Pattern.compile("\"Id\":\"(.*?)\"\\,");
-					Matcher m = p.matcher(patientGroup);
-
-					if (m.find()) {
-						patientGroupValue = m.group(1).trim();
-						System.out.println(patientGroupValue);
-					}
-					// String patientSourceOrgValue = manageClaim.getSourceOrganization();
-					if (patientGroupValue != null || patientGroupValue != " " || claimAdjGroupValue != null
-							|| claimAdjGroupValue != "") {
-						if (patientGroupValue.equals(claimAdjGroupValue)) {
-							Assert.assertTrue(true);
+				boolean isreadOnly = manageClaim.isAttribtuePresent();
+				if (claimAdjusterid != null || claimAdjusterid != " " || userId != null || userId != " ") {
+					if (claimAdjusterid.equals(userId)) {
+						if (isreadOnly) {
+							System.out.println("form is readonly hence cannot edit records");
 						} else {
-							Assert.assertFalse(false);
+							System.out.println("form is not readonly hence can edit records");
 						}
-					}
 
+					} else {
+						Assert.assertFalse(false);
+					}
 				}
+				driver.navigate().back();
 			}
+
 		} else {
 			System.out.println(getUserName + " falls under different role ");
 
 		}
 
 	}
+
+	/*
+	 * @Test(priority = 0, description =
+	 * "Verify list of claim records in manage claim page") public void
+	 * verifyListOfClaimRecord() {
+	 * 
+	 * loginPage = new LoginPage(driver);
+	 * loginPage.loginToApplication(ObjectReader.reader.getUserName(),
+	 * ObjectReader.reader.getPassword());
+	 * 
+	 * manageClaim = new ClaimAdjusterManageClaim(driver);
+	 * manageClaim.verifyManageClaimView();
+	 * 
+	 * getUserName = manageClaim.getLoggedInUserName();
+	 * 
+	 * String url = "https://prodigyservicesdev.powerappsportals.com/user-details/";
+	 * String keyValue = manageClaim.getHTMLResponse(url); JSONObject jsonObject =
+	 * new JSONObject(keyValue);
+	 * 
+	 * userRole = jsonObject.get("webRole").toString(); if
+	 * (userRole.contains("Claim Adjuster")) { JSONArray tsmresponse = (JSONArray)
+	 * jsonObject.get("groups"); for (int i = 0; i < tsmresponse.length(); i++) {
+	 * claimAdjGroupValue = tsmresponse.getString(i).toString(); List<WebElement>
+	 * patientGroup = manageClaim.getGroupId(); for (int j = 0; j <
+	 * patientGroup.size(); j++) { String getGroupNameId =
+	 * patientGroup.get(j).getAttribute("data-value");
+	 * 
+	 * Pattern p = Pattern.compile("\"Id\":\"(.*?)\"\\,"); Matcher m =
+	 * p.matcher(getGroupNameId); if (m.find()) { patientGroupValue =
+	 * m.group(1).trim(); }
+	 * 
+	 * if (patientGroupValue != null || patientGroupValue != " " ||
+	 * claimAdjGroupValue != null || claimAdjGroupValue != " ") { if
+	 * (patientGroupValue.equals(claimAdjGroupValue)) { Assert.assertTrue(true); }
+	 * else { Assert.assertFalse(false); } } }
+	 * 
+	 * }
+	 * 
+	 * } else { System.out.println(getUserName + " falls under different role ");
+	 * 
+	 * }
+	 * 
+	 * }
+	 */
 }
